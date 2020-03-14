19df68d3777655b5da193b1e823da15e4ee354c2
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-03-14 01:46:17 -0400

    el elemento podemos manejarlo como fragment, tambien se puede remplazar por <> </> vacios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React,{Fragment} from 'react';
 import './styles/styles.scss';
 
 const App = () => (
   //segunda regla... solo puede devolver una cosa
-  <div>
+  <Fragment>
     <h1>Hola Mundo2s</h1>
     <p>Saludo</p>
-  </div>
+  </Fragment>
 )
 
 export default App;
+
+
+// reglas JSX
+// 1: Toda etiqueta debe cerrarce
+// 2: Los componentes deben devolver un solo elemento padre
