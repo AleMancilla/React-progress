1ea8ba9bf9a4f06335d92d54675781ff80c6ed45
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-03-14 01:40:36 -0400

    segunda regla... solo puede devolver una cosa en el metodo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './styles/styles.scss';
 
-const App = () => <h1>hola mundo</h1>
+const App = () => (
+  //segunda regla... solo puede devolver una cosa
+  <div>
+    <h1>Hola Mundo2s</h1>
+    <p>Saludo</p>
+  </div>
+)
 
 export default App;
