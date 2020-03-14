b44d9ba8abdfb44e21f086632e55f6135434fd80
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-03-14 02:02:19 -0400

    reglas que debemos tener en cuenta

--- src/App.js
+++ src/App.js
@@ -15,3 +15,9 @@ export default App;
 // reglas JSX
 // 1: Toda etiqueta debe cerrarce
 // 2: Los componentes deben devolver un solo elemento padre
+// 3: Apoyarse de los Fragments cuando necesito devolver 2 elementos
+// 4: Fragment => <> hijos </>
+// 5: Etiqueta img siempre se cierra
+// 6: class => className por que el elemento parece html pero NO ES HTML
+// 7: for => htmlFor
+// 8: no podemos usar ni IF ni else ni while
