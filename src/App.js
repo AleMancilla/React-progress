79c4c7e744d6da295d58cb611a7ff72ea0680989
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-03-15 01:37:38 -0400

    utilizamos un objeto y un ciclo tipo map para crear distintos cursos en frontend

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React from 'react';
 import './styles/styles.scss';
 import Card from './Curso';
 
+
+const cursos = [
+  {
+    "titulo":"React desde cero", "image":"algo.jpg","precio":50,"profesor":"Alejandro Mancilla"
+  },{
+    "titulo":"HTML desde cero", "image":"algo.jpg","precio":80,"profesor":"Alejandro "
+  },{
+    "titulo":"GO desde cero", "image":"algo.jpg","precio":40,"profesor":" Mancilla"
+  },{
+    "titulo":"FLUTTER desde cero", "image":"algo.jpg","precio":30,"profesor":"Alejandro Mancilla"
+  }
+]
+
+
 const App = () => (
   //segunda regla... solo puede devolver una cosa
   <>
     <div className="main-banner img-container l-section" id="main-banner">
     <div classNameName="ed-grid lg-grid-6">
       <div className="lg-cols-4 lg-x-2">
-        <img className="main-banner__img" src="https://i.pinimg.com/originals/69/55/8b/69558b1a4b38f75fa37d4978a7151fb0.jpg"  />
+        <img className="main-banner__img" src="https://i.pinimg.com/originals/69/55/8b/69558b1a4b38f75fa37d4978a7151fb0.jpg"  alt="ninguno"/>
         <div className="main-banner__data s-center">
           <p className="t2 s-mb-0">Título del banner</p>
           <p> Subtítulo del banner2</p>
@@ -19,21 +33,10 @@ const App = () => (
   </div>
 
   <div className="ed-grid m-grid-3">
-  <Card 
-    title="React desde cero"
-    imagen="https://i.blogs.es/3cc483/1-hsisluifmo6kblfpoktlow/450_1000.jpeg"
-    precio="69 USD"
-    />
 
-  <Card 
-    title="HTML desde cero"
-    imagen="https://i.blogs.es/3cc483/1-hsisluifmo6kblfpoktlow/450_1000.jpeg"
-    precio="69 USD"
-    />
-
-    <Card />
-    <Card />
-    
+    {
+      cursos.map( curs => <Card title = {curs.titulo} imagen = {curs.image} precio = {curs.precio} profesor = {curs.profesor} /> )// no usamos forech por que solo recorre en cambio map construlle un nuevo array
+    }
 
   </div>
 
