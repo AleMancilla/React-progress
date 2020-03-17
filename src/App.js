a69db525c67f6233d11bd743501339199088a60d
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-03-17 18:08:18 -0400

    adicionamos el banner como jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/styles.scss';
 import Card from './Curso';
+import Banner from './Banner'
 
 
 const cursos = [
@@ -19,18 +20,8 @@ const cursos = [
 const App = () => (
   //segunda regla... solo puede devolver una cosa
   <>
-    <div className="main-banner img-container l-section" id="main-banner">
-    <div classNameName="ed-grid lg-grid-6">
-      <div className="lg-cols-4 lg-x-2">
-        <img className="main-banner__img" src="https://i.pinimg.com/originals/69/55/8b/69558b1a4b38f75fa37d4978a7151fb0.jpg"  alt="ninguno"/>
-        <div className="main-banner__data s-center">
-          <p className="t2 s-mb-0">Título del banner</p>
-          <p> Subtítulo del banner2</p>
-          <a href="http://www.google.com" className="button">Botón del banner</a>
-        </div>
-      </div>
-    </div>
-  </div>
+    <Banner />
+
 
   <div className="ed-grid m-grid-3">
 
