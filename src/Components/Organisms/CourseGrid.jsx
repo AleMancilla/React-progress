9bf5349ad1c2f0a5c0d4fe35c697d46ea0a96b0a
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2020-03-25 12:08:07 -0400

    consumiendo api rest desde github

--- src/Components/Organisms/CourseGrid.jsx
+++ src/Components/Organisms/CourseGrid.jsx
@@ -1,52 +1,48 @@
 import React from 'react'
 import Card from '../Molecules/Curso'
+import { Component } from 'react'
+import axios from 'axios'
 
-const cursos = [
-    {
-        "id":1,
-        "titulo":"React desde cero", 
-        "image":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2020-03/Go-desde-cero.png",
-        "precio":50,
-        "profesor":"Alejandro Mancilla"
-    },
-    {
-        "id":2,
-      "titulo":"HTML desde cero", 
-      "image":"https://res.cloudinary.com/edteam/image/upload/v1583002082/Postgresql-v03.png"
-      ,"precio":80,
-      "profesor":"Alejandro "
-    },
-    {
-        "id":3,
-      "titulo":"GO desde cero", 
-      "image":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2020-01/gcp.png",
-      "precio":40,
-      "profesor":" Mancilla"
-    },
-    {
-        "id":4,
-      "titulo":"FLUTTER desde cero", 
-      "image":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2020-02/scrum_normal-v05.png",
-      "precio":30,
-      "profesor":"Alejandro Mancilla"
-    }
-  ]
 
-const CourseGrid = () => (
-<div className="ed-grid m-grid-4">
-    {
-        cursos.map(c =>(
-            <Card 
-                key = {c.id}
-                id = {c.id}
-                title = {c.titulo}
-                imagen = {c.image}
-                precio = {c.precio}
-                profesor = {c.profesor}
-            />
-        ))
+class CourseGrid extends Component {
+
+  constructor (props){
+    super(props)
+
+    this.state = {
+      cursos: []
     }
-</div>
-)
+  }
+
+  componentDidMount(){
+    axios.get('http://my-json-server.typicode.com/AleMancilla/AleMancilla/cursos')
+    .then(resp => this.setState({
+      cursos: resp.data
+    }))
+  } 
+
+  render(){
+
+    const{ cursos } = this.state
+
+    return(
+      <div className="ed-grid m-grid-4">
+        {
+            cursos.map(c =>(
+                <Card 
+                    key = {c.id}
+                    id = {c.id}
+                    title = {c.titulo}
+                    imagen = {c.image}
+                    precio = {c.precio}
+                    profesor = {c.profesor}
+                />
+            ))
+        }
+    </div>
+    )
+  }
+}
+
 
 export default CourseGrid
--- src/Components/Pages/Course.jsx
+++ src/Components/Pages/Course.jsx
@@ -3,28 +3,28 @@ import React from 'react'
 const cursos = [
     {
         "id":1,
-        "titulo":"React desde cero", 
+        "titulo":"React desde cerooooo", 
         "image":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2020-03/Go-desde-cero.png",
         "precio":50,
         "profesor":"Alejandro Mancilla"
     },
     {
         "id":2,
-      "titulo":"HTML desde cero", 
+      "titulo":"HTML desde ceroooo", 
       "image":"https://res.cloudinary.com/edteam/image/upload/v1583002082/Postgresql-v03.png"
       ,"precio":80,
       "profesor":"Alejandro "
     },
     {
         "id":3,
-      "titulo":"GO desde cero", 
+      "titulo":"GO desde ceroooo", 
       "image":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2020-01/gcp.png",
       "precio":40,
       "profesor":" Mancilla"
     },
     {
         "id":4,
-      "titulo":"FLUTTER desde cero", 
+      "titulo":"FLUTTER desde ceroooo", 
       "image":"https://drupal.ed.team/sites/default/files/styles/16_9_medium/public/imagenes-cdn-edteam/2020-02/scrum_normal-v05.png",
       "precio":30,
       "profesor":"Alejandro Mancilla"
